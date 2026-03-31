66aa82ca83561ab96e6b7df68ab85db23669e8f0
ishu <ishukumar201201@gmail.com>
2026-03-31 10:56:44 -0700

    commit karo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 VANTA.WAVES({
-  el: "#vanta-bg", // Is container mein animation chalegi
+  el: "#vanta-bg",
   mouseControls: true,
   touchControls: true,
   gyroControls: false,
@@ -7,11 +7,9 @@ VANTA.WAVES({
   minWidth: 200.00,
   scale: 1.00,
   scaleMobile: 1.00,
-  
-  // VIDEO WALA COLOR PALETTE: Purple aur Blue ka merge
-  color: 0x6e289c, // Main Purple color like TIDY
-  shininess: 65.00,
-  waveHeight: 25.00,
-  waveSpeed: 0.75,
-  zoom: 1.00
+  color: 0x220c3b,       // Dark Purple Base
+  shininess: 40.00,      // Chamak thodi kam premium look ke liye
+  waveHeight: 35.00,     // Lehrein thodi unchi
+  waveSpeed: 0.50,       // Speed thodi slow aur smooth
+  zoom: 0.65             // Zoom out taaki pattern bada dikhe
 })
